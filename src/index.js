4780970d4bf8803aa16329308f90cd84701b8d48
Anastasiya <anastasiya.baranouskaya@gmail.com>
2019-02-20 21:59:58 +0300

    love triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var number = 0;
+  var j = 0;
+  while (j < preferences.length){
+    var i = preferences.splice(0, 3);
+    if (i[1] - i[0] == 1 && i[0] - i[2] == 1){
+      number += 1;
+    };
+  };
+  return number;
 };
+module.exports[2,3,1]
